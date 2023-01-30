e2f33e87162b7d0aa7f23bcd5684ed2e973cb451
Mariano Goldman <marianogoldman@users.noreply.github.com>
2023-01-30 10:34:56 -0300

    feat: check signed-fetch metadata (#40)

--- src/controllers/handlers/commsAdapterHandler.ts
+++ src/controllers/handlers/commsAdapterHandler.ts
@@ -1,7 +1,6 @@
 import { HandlerContextWithPath } from '../../types'
 import { IHttpServerComponent } from '@well-known-components/interfaces'
 import { DecentralandSignatureContext } from 'decentraland-crypto-middleware/lib/types'
-
 export async function commsAdapterHandler(
   context: HandlerContextWithPath<'commsResolver', '/get-comms-adapter/:roomId'> & DecentralandSignatureContext<any>
 ): Promise<IHttpServerComponent.IResponse> {
@@ -9,12 +8,23 @@ export async function commsAdapterHandler(
     components: { commsResolver }
   } = context
 
-  const fixedAdapter = await commsResolver.resolveComms(context.verification!.auth, context.params.roomId)
+  if (!validateMetadata(context.verification!.authMetadata)) {
+    return {
+      status: 400,
+      body: {
+        message: 'Access denied, invalid metadata'
+      }
+    }
+  }
 
   return {
     status: 200,
     body: {
-      fixedAdapter
+      fixedAdapter: await commsResolver.resolveComms(context.verification!.auth, context.params.roomId)
     }
   }
+}
+
+function validateMetadata(metadata: Record<string, any>): boolean {
+  return metadata.signer === 'dcl:explorer' && metadata.intent === 'dcl:explorer:comms-handshake'
 }
--- test/integration/comms-adapter-handler.spec.ts
+++ test/integration/comms-adapter-handler.spec.ts
@@ -50,6 +50,44 @@ test('comms adapter handler /get-comms-adapter/:roomId', function ({ components 
 })
 
 test('comms adapter handler /get-comms-adapter/:roomId', function ({ components }) {
+  it('fails when signed-fetch request metadata is correct', async () => {
+    const { localFetch } = components
+
+    const identity = await getIdentity()
+
+    const path = '/get-comms-adapter/myRoom'
+    const actualInit = {
+      method: 'POST',
+      headers: {
+        ...getAuthHeaders(
+          'post',
+          path,
+          {
+            origin: 'https://play.decentraland.org'
+          },
+          (payload) =>
+            Authenticator.signPayload(
+              {
+                ephemeralIdentity: identity.ephemeralIdentity,
+                expiration: new Date(),
+                authChain: identity.authChain.authChain
+              },
+              payload
+            )
+        )
+      }
+    }
+
+    const r = await localFetch.fetch(path, actualInit)
+
+    expect(r.status).toEqual(400)
+    expect(await r.json()).toEqual({
+      message: 'Access denied, invalid metadata'
+    })
+  })
+})
+
+test('comms adapter handler /get-comms-adapter/:roomId', function ({ components }) {
   it('fails when request is not a signed-fetch one', async () => {
     const { localFetch } = components
 
