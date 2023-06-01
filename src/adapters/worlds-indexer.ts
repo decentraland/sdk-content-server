f0c4daacf56181c01ecfec735f834ca33b27dd30
Mariano Goldman <marianogoldman@users.noreply.github.com>
2023-06-01 17:12:02 -0300

    refactor: fetch data concurrently to speed up indexing (#102)

--- src/adapters/worlds-indexer.ts
+++ src/adapters/worlds-indexer.ts
@@ -2,6 +2,7 @@ import { AppComponents, IWorldsIndexer, WorldData, WorldsIndex } from '../types'
 import { bufferToStream, streamToBuffer } from '@dcl/catalyst-storage/dist/content-item'
 import { stringToUtf8Bytes } from 'eth-connect'
 import { ContentMapping } from '@dcl/schemas/dist/misc/content-mapping'
+import PQueue from 'p-queue'
 
 const GLOBAL_INDEX_FILE = 'global-index.json'
 
@@ -12,34 +13,52 @@ export async function createWorldsIndexerComponent({
 }: Pick<AppComponents, 'logs' | 'storage' | 'worldsManager'>): Promise<IWorldsIndexer> {
   const logger = logs.getLogger('worlds-indexer')
 
+  async function fetchWorldsData(deployedWorldsNames: string[]) {
+    const queue = new PQueue({ concurrency: 10 })
+
+    const byName = new Map<string, WorldData | undefined>()
+    for (const worldName of deployedWorldsNames) {
+      queue
+        .add(async () => {
+          const entity = await worldsManager.getEntityForWorld(worldName)
+          if (!entity) {
+            return
+          }
+          const thumbnailFile = entity.content.find(
+            (content: ContentMapping) => content.file === entity.metadata?.display?.navmapThumbnail
+          )
+          byName.set(worldName, {
+            name: worldName,
+            scenes: [
+              {
+                id: entity.id,
+                title: entity.metadata?.display?.title,
+                description: entity.metadata?.display?.description,
+                thumbnail: thumbnailFile?.hash,
+                pointers: entity.pointers,
+                runtimeVersion: entity.metadata?.runtimeVersion,
+                timestamp: entity.timestamp
+              }
+            ]
+          })
+        })
+        .catch((error) => {
+          logger.error(`Error fetching data for world ${worldName}: ${error.message}`)
+        })
+    }
+
+    await queue.onIdle()
+
+    return byName
+  }
+
   async function createIndex(): Promise<WorldsIndex> {
     logger.info('Creating index of all the data from all the worlds deployed in the server')
     const deployedWorldsNames = await worldsManager.getDeployedWorldsNames()
-    const index: WorldData[] = []
-
-    for (const worldName of deployedWorldsNames) {
-      const entity = await worldsManager.getEntityForWorld(worldName)
-      if (!entity) {
-        continue
-      }
-      const thumbnailFile = entity.content.find(
-        (content: ContentMapping) => content.file === entity.metadata?.display?.navmapThumbnail
-      )
-      index.push({
-        name: worldName,
-        scenes: [
-          {
-            id: entity.id,
-            title: entity.metadata?.display?.title,
-            description: entity.metadata?.display?.description,
-            thumbnail: thumbnailFile?.hash,
-            pointers: entity.pointers,
-            runtimeVersion: entity.metadata?.runtimeVersion,
-            timestamp: entity.timestamp
-          }
-        ]
-      })
-    }
+    const byName = await fetchWorldsData(deployedWorldsNames)
+    const index: WorldData[] = deployedWorldsNames
+      .filter((worldName) => byName.has(worldName) && byName.get(worldName) !== undefined)
+      .map((worldName) => byName.get(worldName)!)
 
     const indexData: WorldsIndex = { index, timestamp: Date.now() }
     await storage.storeStream(GLOBAL_INDEX_FILE, bufferToStream(stringToUtf8Bytes(JSON.stringify(indexData))))
